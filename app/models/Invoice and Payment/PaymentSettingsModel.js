62da808115d6c5e16aadcf5832a062597bcf326a
mohammad-nayem <nishanahmed13913@gmail.com>
2024-12-01 11:53:05 +0600

    Payment Settings DB model create

---
+++ app/models/Invoice and Payment/PaymentSettingsModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const DataSchema = mongoose.Schema(
+    {
+        store_id: {type: String, required: true},
+        store_password: {type: String, required: true},
+        currency: {type: String, required: true},
+        success_url: {type: String, required: true},
+        fail_url: {type: String, required: true},
+        cancel_url: {type: String, required: true},
+        ipn_url: {type: String, required: true},
+        init_url: {type: String, required: true},
+    },
+    {timestamps: true, versionKey: false}
+)
+
+const PaymentSettingsModel = mongoose.model('paymentsettings', DataSchema);
+
+
+// Export Data Model
+export default PaymentSettingsModel;
