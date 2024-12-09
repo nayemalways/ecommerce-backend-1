ed3887d9a3ff0570d5faf3ad57c59818c0118858
mohammad-nayem <nishanahmed13913@gmail.com>
2024-12-10 00:12:03 +0600

    JWT Token Endoced and Decode features added

--- app/utility/TokenHelper.js
+++ app/utility/TokenHelper.js
@@ -1,1 +1,23 @@
-// Token Encode feature will be included
+import jwt from 'jsonwebtoken';
+import { JWT_EXPIRATIONS_TIME, JWT_SECRET } from '../config/config.js';
+
+
+// ENCODE TOKEN 
+export const TokenEncode = (email, user_id) => {
+    const KEY = JWT_SECRET;
+    const EXPIRE = {expiresIn: JWT_EXPIRATIONS_TIME};
+    const PAYLOAD = {email: email, user_id: user_id};
+    
+
+    return jwt.sign(PAYLOAD, KEY, EXPIRE);
+};
+
+
+// DECODE TOKEN 
+export const DecodeToken = (token) => {
+    try{
+        return jwt.verify(token, JWT_SECRET);
+    }catch(error){
+        return null;
+    }
+}
