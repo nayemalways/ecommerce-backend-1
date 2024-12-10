75ed407f25a4d59fa4d87207f7169aa496909ef0
mohammad-nayem <nishanahmed13913@gmail.com>
2024-12-10 14:07:07 +0600

    User cookie set

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,3 +1,4 @@
+import { json } from "express";
 import {UserOTPService, VerifyOTPService, UserLogoutService, CreateProfileService, UpdateProfileService, ReadProfileService} from "../Services/UserServices.js";
 
 export const UserOTP = async (req, res) => {
@@ -8,7 +9,20 @@ export const UserOTP = async (req, res) => {
 
  export const OTPVerifyLogin = async (req, res) => {
     const result = await VerifyOTPService(req);
-    res.json(result);
+
+    if(result['status'] === 'Success') {
+
+        // Set cookie option
+        const cookieOptions = {expires: new Date(Date.now() + 24 * 60 * 60 * 1000), httpOnly: false};
+
+        // Set cookie
+        res.cookie("token", result['Token'], cookieOptions);
+
+        res.json(result)
+    }else {
+
+        res.json(result);
+    }
 } 
 
 
