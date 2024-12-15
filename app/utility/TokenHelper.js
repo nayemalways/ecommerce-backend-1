f79ba6ebbe575877df286c95bece98d941d0f0b1
mohammad-nayem <nishanahmed13913@gmail.com>
2024-12-15 12:33:42 +0600

    JWT token expirations modified

--- app/utility/TokenHelper.js
+++ app/utility/TokenHelper.js
@@ -5,7 +5,7 @@ import { JWT_EXPIRATIONS_TIME, JWT_SECRET } from '../config/config.js';
 // ENCODE TOKEN 
 export const TokenEncode = (email, user_id) => {
     const KEY = JWT_SECRET;
-    const EXPIRE = {expiresIn: JWT_EXPIRATIONS_TIME};
+    const EXPIRE = {expiresIn: '7D'};
     const PAYLOAD = {email: email, user_id: user_id};
     
 
